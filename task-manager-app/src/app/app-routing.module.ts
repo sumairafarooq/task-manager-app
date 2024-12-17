ea79d1b1309e58790c45a1799e02790a5f51c14a
sumaira <sumairafarooq8@gmail.co>
2024-12-17 09:56:24 +0530

    login and sign up

--- task-manager-app/src/app/app-routing.module.ts
+++ task-manager-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import  { LoginComponent} from './login/login.component'
+import { SignupComponent } from './signup/signup.component';
+const routes: Routes = [{ path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login by default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
